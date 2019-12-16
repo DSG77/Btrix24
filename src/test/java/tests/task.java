a951b5b8e530cd81cf744fbe96ea4444e0b8a3fa
DSG77 <56004336+DSG77@users.noreply.github.com>
2019-12-16 17:41:00 +0100

    updated task class

--- src/test/java/tests/task.java
+++ src/test/java/tests/task.java
@@ -2,5 +2,10 @@ package tests;
 
 public class task {
 
-
+public static void main(String[] args) {
+  System.out.println("Hello world");
+  
+        //this line is added from GitHub,see you intelliJ
+}
+  
 }
